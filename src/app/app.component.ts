35c371adba8099f6bf74ac4e89f04c5966375086
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-02-10 23:05:58 +0300

    Add ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule, SelectButtonOptionClickEvent } from 'primeng/selectbutton';
 
 interface Country {
   name: string;
@@ -14,6 +16,11 @@ interface City {
   code: string;
 }
 
+interface Language {
+  name: string;
+  code: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -21,7 +28,9 @@ interface City {
     ButtonModule,
     FormsModule,
     DropdownModule,
-    RouterOutlet
+    RouterOutlet,
+    SelectButtonModule,
+    TranslateModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -55,7 +64,14 @@ export class AppComponent implements OnInit {
     ]]
   ]);
 
+  languages: Language[] = [{name: 'English', code: 'en'}, {name: 'German', code: 'de'}];
+  selectedLanguage = 'en';
+
+  constructor(private translateService: TranslateService) {}
+
   ngOnInit() {
+    this.translateService.use('en');
+
     this.countries = [
       {name: 'Australia', code: 'AU'},
       {name: 'Germany', code: 'DE'},
@@ -95,5 +111,9 @@ export class AppComponent implements OnInit {
 
     this.dynamicCities = [];
     this.dynamicSelectedCity = undefined;
+  }
+
+  changeLanguage(event: SelectButtonOptionClickEvent) {
+    this.translateService.use(event.option.code);
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 import { routes } from './app.routes';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
